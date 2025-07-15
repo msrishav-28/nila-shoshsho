b7c247e94ce2f2d310ff6778b8aca3231ea20a6f
M S Rishav Subhin <158768223+msrishav-28@users.noreply.github.com>
2025-07-15 09:13:27 +0000

    modified:   MobileApp/i18n.js 	new file:   MobileApp/locales/bn.json 	modified:   MobileApp/locales/en.json 	modified:   MobileApp/locales/hi.json 	new file:   MobileApp/locales/kn.json 	new file:   MobileApp/locales/ml.json 	modified:   MobileApp/locales/mr.json 	modified:   MobileApp/locales/ta.json   new file:   MobileApp/locales/te.json

--- MobileApp/i18n.js
+++ MobileApp/i18n.js
@@ -7,12 +7,20 @@ import en from './locales/en.json';
 import hi from './locales/hi.json';
 import mr from './locales/mr.json';
 import ta from './locales/ta.json';
+import bn from './locales/bn.json';
+import kn from './locales/kn.json';
+import te from './locales/te.json';
+import ml from './locales/ml.json';
 
 const resources = {
   en: {translation: en},
   hi: {translation: hi},
   mr: {translation: mr},
   ta: {translation: ta},
+  bn: {translation: bn},
+  kn: {translation: kn},
+  te: {translation: te},
+  ml: {translation: ml},
 };
 
 const fallbackLng = 'en';
